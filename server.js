0b9fe7c22e6a8dc2d13a861f6ef0703747f7f7d1
Oleksii Lobok <zhuyka1993@gmail.com>
2024-11-12 14:28:44 +0200

    fix routes

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.get("/readMail", requireLogin, (req, res) => {
 });
 
 // Використання роутів для UserMail
-app.use("/api/mails", userMailRoutes);
+app.use("/api", userMailRoutes);
 
 // Обслуговування статичних файлів з папки 'public'
 app.use(express.static(path.join(__dirname, "public")));
